bb37fb0a67b182ded46f8ca31169324e4c5280c5
Felipe Coelho <felipecrs04@gmail.com>
2024-04-22 23:59:38 -0300

    atualizarErrado

--- my-app/pages/profile.js
+++ my-app/pages/profile.js
@@ -3,7 +3,7 @@ import { getSession } from 'next-auth/client';
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
-  return <UserProfile />;
+  return <UserProfilee />;
 }
 
 export async function getServerSideProps(context) {
